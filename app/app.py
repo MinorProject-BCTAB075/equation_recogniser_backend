f73f4646be6418563d97caaa4758acf3d336542f
Kuu44 <kushal-shrestha@hotmail.com>
2022-03-18 21:11:59 +0545

    updated app

--- app/app.py
+++ app/app.py
@@ -2,14 +2,8 @@ from flask import Blueprint, request, json, jsonify
 
 views = Blueprint('views', __name__)
 
-@views.route('/register', methods=["GET", "POST"])
-def register():
+@views.route('/predict', methods=["GET", "POST"])
+def predict():
     d={}
     if request.method =="POST":
         return jsonify(["Register success"])
-
-@views.route('/login', methods=["GET", "POST"])
-def login():
-    d = {}
-    if request.method =="POST":
-        return jsonify([ "success"])
--- app/hello.py
+++ app/hello.py
@@ -5,25 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     return render_template('about.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-@app.route('/signup', methods=['POST'])
-def signup_post():
-    print(dir(request))    
-    email = request.form.get('email')
-    print(email)
-    name = request.form.get('name')
-    print(name)
-    password = request.form.get('password')
-    print(password)
-    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
